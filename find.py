ae8e31a56fc83cc1651548fd4be7719788c1cbed
koftee <90131331+koftee@users.noreply.github.com>
2023-10-15 16:57:58 +0300

    find.py

--- find.py
+++ find.py
@@ -1,9 +1,12 @@
 def classify(number):
     toplam = 0
     if number <1:
+        #checks the value of the number
         raise ValueError("Classification is only possible for positive integers.")
     for i in range(1,number):
+        #find the divisors of the number
         if number % i == 0:
+            #find the sum of the divisors
             toplam +=i
     if toplam == number:
         return "perfect"
